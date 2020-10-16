63390871a92ce217acbf504a951484e0df3d010d
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-10-16 18:47:07 -0400

    chore: adds lts scenarios to ember-try up through current

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,17 +1,25 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const typeTests = require('./ember-try-typescript');
 
-const typeScriptScenarios = typeTests.scenarios.map((s) => ({
-  ...s,
-  command: typeTests.command,
-}));
-
-module.exports = async function () {
+module.exports = async function() {
   return {
     useYarn: true,
     scenarios: [
+      {
+        name: 'ember-lts-2.18',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'jquery-integration': true,
+          }),
+        },
+        npm: {
+          devDependencies: {
+            '@ember/jquery': '^0.5.1',
+            'ember-source': '~2.18.0',
+          },
+        },
+      },
       {
         name: 'ember-lts-3.4',
         npm: {
@@ -32,7 +40,7 @@ module.exports = async function () {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
-            'ember-source': '~3.12.0',
+            'ember-source': '~3.16.0',
           },
         },
       },
@@ -76,6 +84,16 @@ module.exports = async function () {
           },
         },
       },
+      // The default `.travis.yml` runs this scenario via `yarn test`,
+      // not via `ember try`. It's still included here so that running
+      // `ember try:each` manually or from a customized CI config will run it
+      // along with all the other scenarios.
+      {
+        name: 'ember-default',
+        npm: {
+          devDependencies: {},
+        },
+      },
       {
         name: 'ember-default-with-jquery',
         env: {
@@ -89,26 +107,6 @@ module.exports = async function () {
           },
         },
       },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          ember: {
-            edition: 'classic',
-          },
-        },
-      },
-
-      // Include the type tests, while still leaving them in their own file so
-      // they can be run independently, for example to run all the type tests but
-      // *only* the type tests locally.
-      ...typeScriptScenarios,
     ],
   };
 };
